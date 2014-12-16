6b082469fe092fc96417511692e867999e8c1dfc
goekhan <gguerkan88@gmail.com>
2014-12-16 12:54:06 +0100

    testcommit umwandeln funktion

---
+++ umwandeln/src/umwandeln/Umwandel.java
@@ -0,0 +1,23 @@
+package umwandeln;
+
+public class Umwandel {
+
+	
+	
+	
+	
+	
+	
+	
+
+	public static void main(String[] args) {
+		
+		float wandelzahl =  0.078f ;
+		int intewandelzahl = (int)(wandelzahl *1000);
+	
+		System.out.println(intewandelzahl);
+
+	}
+
+}
+
